a0589b2545f100fc494d6fcff5450b33bb2effd6
purkayasta <pritompurkayasta@gmail.com>
2021-10-29 03:27:23 +0600

    About Me Page Added

---
+++ src/components/nav.svelte
@@ -0,0 +1,11 @@
+<nav>
+	<div class="flex place-content-center space-x-48">
+		<div class="p-2 bg-yellow-300">
+			<i class="bi bi-stopwatch-fill" />
+			<a href="/">Focus Mode</a>
+		</div>
+		<div class="p-2 bg-purple-300">
+			<a href="/about">About Me</a>
+		</div>
+	</div>
+</nav>
---
+++ src/routes/about.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>About Me! 🙌</title>
+</svelte:head>
+
+<div>
+	<div class="w-3/4 lg:w-1/2 mx-auto rounded-md bg-gray-200 shadow-2xl m-20 p-10 text-center">
+		<img src="static/profilepic.jpg" class="w-80 mx-auto rounded-lg mb-4" alt="avatar" />
+		<h1 class="text-3xl">Pritom Purkayasta</h1>
+		<p class="text-gray-500 pb-4">Software Engineer</p>
+		<p class="text-gray-700 mb-6">Hello Friend!</p>
+
+		<div class="grid grid-cols-1 md:grid-cols-3 grid-flow-row gap-6">
+			<div class="px-4 py-2 text-white bg-black rounded-md mr-4 hover:bg-pink-700">
+				<a href="https://www.github.com/purkayasta">Github</a>
+			</div>
+			<div class="px-4 py-2 bg-black text-white rounded-md mr-4 hover:bg-green-700">
+				<a href="https://www.linkedin.com/in/purkayasta/">LinkedIn</a>
+			</div>
+			<div class="px-4 py-2 bg-black text-white rounded-md mr-4 hover:bg-purple-800">
+				<a href="https://pritompurkayasta.me/">Blog</a>
+			</div>
+		</div>
+	</div>
+</div>
