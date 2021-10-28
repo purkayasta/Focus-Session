bc387c618868f09e9e780fbe267f8d37fa3fa46f
purkayasta <pritompurkayasta@gmail.com>
2021-10-29 03:27:02 +0600

    Tailwind CSS Added and Configured

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../index.css';
+	import Navbar from '../components/nav.svelte';
+</script>
+
+<div class="">
+	<Navbar />
+	<slot />
+</div>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
